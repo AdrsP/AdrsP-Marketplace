22fff86661adb61a399405a520a4c93f1cb43a9a
Andres Eduardo Parra Berrios <144963769+AdrsP@users.noreply.github.com>
2024-03-12 02:38:50 +0000

    Se creo la base de la pagina, desde los modelos y rutas en el backend para items del mercado, como las paginas y el store del front

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,43 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username =db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    password_hash = db.Column(db.String(60), unique=False, nullable=False) #for security we will use an encripted password
+    budget = db.Column(db.Float, nullable=False, default=1000) #default just gives an default value to the variable/column
+    items = db.relationship('Item', backref='owned_user', lazy=True) 
+    """the property backref: owned_user allows an "external column that shows which 
+    user owns the product, and the lazy will allow to load all the elements at the
+    same time"""
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+            "budget": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    bardcode = db.Column(db.String(12), nullable=False, unique=True)
+    price = db.Column(db.Float(40), nullable=False, unique=False)
+    description = db.Column(db.String(1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "barcode": self.bardcode,
+            "price": self.price,
+            "description": self.description,
+            "owner": self.owner,
         }
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid justify-content-between">
+        <Link className="navbar-brand" to="/">
+          MarketPlace
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-between"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/market">
+                Market
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                Register
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
